9f49c9f74f3ba45121a75be99261a5e8f42349c7
GoldenMEmre <goldenpie60@gmail.com>
2023-03-31 11:35:43 +0300

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,25 @@
+public class G02 {
+
+    /*
+    Git-Github'da projelerimizin üzerinde çalışırken temelde 3 adımda işlem yapıyoruz
+
+        1- Projelerimizi/Dosyalarımızı git ile ilişkilendiriyoruz
+
+                git init
+
+        2- projemizde/dosyamızda bulunnanları versiyon oluşturuyorduk
+
+            git add .
+            git commit -m "versiyonadi"
+
+        3- github'a yollama/entegrasyonu
+
+            git remote add origin repoUrlAdresi
+            already origin
+            git remote set-url repoUrlAdresi
+            git push
+     */
+        System.out.println("İşlem ekleyelim");
+        System.out.println("HEAD");
+        System.out.println("Master");
+}
